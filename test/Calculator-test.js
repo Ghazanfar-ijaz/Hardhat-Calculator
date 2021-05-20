83d3b918e40f807c8aa7baed00faf753f5746c35
Ghazanfar-ijaz <ghazanfar@outlook.fr>
2021-05-20 23:30:41 +0200

    test fail

--- test/Calculator-test.js
+++ test/Calculator-test.js
@@ -1,39 +1,31 @@
 const { expect } = require('chai');
-const { ethers } = require('ethers');
-
+const { ethers } = require('hardhat');
 
 describe("Calculator", () => {
-  let Calculator, calculator;
+  let calculator;
 
   beforeEach(async () => {
     Calculator = await ethers.getContractFactory("Calculator");
     calculator = await Calculator.deploy();
   });
 
-  describe('Calculation', () => {
-    it('Should add 2 numbers', async function () {
-      const result = await calculator.add(2, 4);
-      expect(result).to.equal(6);
-    });
+  it('Should add 2 numbers', async function () {
+    expect(await calculator.add(2, 4)).to.equal(6);
+  });
 
-    it('Should subtract 2 numbers', async function () {
-      const result = await calculator.sub(9, 4);
-      expect(result).to.equal(5);
-    });
+  it('Should subtract 2 numbers', async function () {
+    expect(await calculator.sub(9, 4)).to.equal(5);
+  });
 
-    it('Should divide 2 numbers', async function () {
-      const result = await calculator.div(20, 2);
-      expect(result).to.equal(10);
-    });
+  it('Should divide 2 numbers', async function () {
+    expect(await calculator.div(20, 2)).to.equal(10);
+  });
 
-    it('Should multiply 2 numbers', async function () {
-      const result = await calculator.mul(6, 3);
-      expect(result).to.equal(18);
-    });
+  it('Should multiply 2 numbers', async function () {
+    expect(await calculator.mul(6, 3)).to.equal(18);
+  });
 
-    it('Should give modulo 2 numbers', async function () {
-      const result = await calculator.mod(2, 3);
-      expect(result).to.equal(2);
-    });
+  it('Should give modulo 2 numbers', async function () {
+    expect(await calculator.mod(2, 3)).to.equal(2);
   });
 });
